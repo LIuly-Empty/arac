c88929d2ab47ca09578c36a2a633cd3a22277f7f
trissera <ikslista.user@gmail.com>
2026-01-30 02:14:36 +0700

    add code

--- AIAC.bash
+++ AIAC.bash
@@ -5,3 +5,9 @@ if [ -d /sys/firmware/efi ]; then
 else
     echo "System is booted in BIOS mode"
 fi
+
+printf "Cai dat arch linux cho nguoi moi:\n
+1. Kiem tra ket noi mang\n
+2. Phan vung o dia\n
+3. Dinh dang o dia\n
+4. Mount va truy cap arch-chroot"
