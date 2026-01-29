462e3ac6ac7ded5dd5a095c0ce4b3e4a48ffb402
trissera <ikslista.user@gmail.com>
2026-01-30 02:01:00 +0700

    add check mode

--- AIAC.bash
+++ AIAC.bash
@@ -1,2 +1,7 @@
 #!/bin/bash
-cfdisk
+echo "check EUFI mode or BIOS mode"
+if [ -d /sys/firmware/efi ]; then
+    echo "System is booted in UEFI mode"
+else
+    echo "System is booted in BIOS mode"
+fi
