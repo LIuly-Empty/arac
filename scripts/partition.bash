07fae11701e43c0c4d5409a378b96f62a2189219
trissera <ikslista.user@gmail.com>
2026-01-30 03:44:54 +0700

    add some features

--- scripts/partition.bash
+++ scripts/partition.bash
@@ -1,18 +1,43 @@
 #!bin/bash
 clear
-printf "\t==============================================
+printf "
+\t==============================================
 \t=          PARTITION DISK SCRIPT             =
 \t==============================================\n"
-printf "\tTien trinh nay se thuc hien phan vung o dia\n\n"
+    for i in {1..5}; do 
+        for s in / - \\ \|; do 
+            printf "\rTien trinh nay se thuc hien phan vung o dia $s" 
+            sleep 0.2 
+        done 
+    done
 
-printf "1. Kiem tra firmware:"
+printf "\n\nKiem tra firmware:"
 if [ -d /sys/firmware/efi ]; then
-    printf "\n-> He thong dang chay o che do UEFI.\n"
+# Phan vung cho UEFi
+    printf "
+-> He thong dang chay o che do UEFI.
+Bat dau phan vung o dia cho UEFI o che do GPT\n"
+    for i in {1..5}; do 
+        for s in / - \\ \|; do 
+            printf "\rĐang xử lý $s" 
+            sleep 0.2 
+        done 
+    done
+    printf "\n\nKiem tra dung luong o dia de phan vung: "
+    lsblk | echo "sda" $(lsblk | grep sda | awk '{print $4}')
     firmware="UEFI"
 else
-    printf "\n-> He thong dang chay o che do BIOS.\n"
+# Phan vung cho BIOS
+    printf "
+-> He thong dang chay o che do BIOS.
+Bat dau phan vung o dia cho BIOS o che do MBR\n"
+    for i in {1..5}; do 
+        for s in / - \\ \|; do 
+            printf "\rĐang xử lý $s" 
+            sleep 0.2 
+        done 
+    done
+    printf "\n\nKiem tra dung luong o dia de phan vung: "
+    lsblk | echo "sda" $(lsblk | grep sda | awk '{print $4}')
     firmware="BIOS"
 fi
-
-printf "2. Kiem tra o dia hien co:"
-df -h
