36eff24beaeea9c30e79d7d67f2ef95d2b79a177
trissera <ikslista.user@gmail.com>
2026-02-15 18:52:16 +0700

    add ..

--- fastrun.bash
+++ fastrun.bash
@@ -7,13 +7,14 @@ clear
 DISK="/dev/sda"
 sgdisk --zap-all $DISK
 
-# Phan vung o sda1 -> EFI System Partition | type: EF00
-sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI System Partition" $DISK
-mkfs.fat -F32 /dev/sda1
-
-# Phan vung o sda2 -> Linux Filesystem | type: 8300
-sgdisk -n 2:0:+20G -t 2:8300 -c 2:"Linux Filesystem" $DISK
-mkfs.ext4 /dev/sda2
+if [ -d /sys/firmware/efi ]; then
+    # Phan vung o sda1 -> EFI System Partition | type: EF00
+    sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI System Partition" $DISK
+    mkfs.fat -F32 /dev/sda1
+else
+    sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"BIOS boot" $DISK
+    mkfs.ext4 /dev/sda1
+fi
 
 # Phan vung o sda3 -> Linux Swap | type: 8200
 sgdisk -n 3:0:+4G -t 3:8200 -c 3:"Linux Swap" $DISK
@@ -21,8 +22,12 @@ mkswap /dev/sda3
 swapon /dev/sda3
 
 # Phan vung o sda4 -> Linux home | type: 8300
-sgdisk -n 4:0:0 -t 4:8300 -c 4:"Linux Home" $DISK
+sgdisk -n 4:0:+5G -t 4:8300 -c 4:"Linux Home" $DISK
 mkfs.ext4 /dev/sda4
+
+# Phan vung o sda2 -> Linux Filesystem | type: 8300
+sgdisk -n 2:0:+20G -t 2:8300 -c 2:"Linux Filesystem" $DISK
+mkfs.ext4 /dev/sda2
 
 echo -e "${TX_GREEN}Phan vung o dia hoan tat!${TX_NC}"
 echo "Nhan nut bat ki de tiep tuc..."
@@ -36,25 +41,11 @@ pacstrap /mnt base base-devel linux linux-firmware vim sudo networkmanager
 systemctl enable NetworkManager --root=/mnt
 genfstab -U /mnt >> /mnt/etc/fstab
 
+cp arac/test.bash /mnt/test.bash
+
 arch-chroot /mnt /bin/bash << EOF
-# Set timezone
-ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
-hwclock --systohc
-# Set locale
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "vi_VN.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-# Set hostname
-echo "archlinux" > /etc/hostname
-
-# Install and configure GRUB
-pacman -Sy grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo -e "${TX_GREEN}Cai dat he dieu hanh va bootloader hoan tat!${TX_NC}"
-echo "Nhan nut bat ki de khoi dong lai..."
-read -n 1 -s
-
+bash /test.bash
 EOF
+
+umount -R /mnt
+reboot
--- test.bash
+++ test.bash
@@ -1,16 +1,25 @@
 #!/bin/bash
 clear
 
-colors=(31 33 32 36 34 35)  # đỏ, vàng, xanh lá, cyan, xanh dương, tím
+# File nay chay trong arch-chroot
+# Set timezone
+ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
+hwclock --systohc
+# Set locale
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "vi_VN.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+# Set hostname
+echo "archlinux" > /etc/hostname
 
-for ((i=0; i<${#text}; i++)); do
-    color=${colors[$((i % ${#colors[@]}))]}
-    printf "\e[1;${color}m%s\e[0m" "${text:$i:1}"
-    sleep 0.1
-done
+# Install and configure GRUB
+pacman -Sy grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
 
-text="==============================================
-=       INSTALL ARCH LINUX AUTOMATION        =
-=============================================="
-echo
-echo -e "\n"
+echo -e "${TX_GREEN}Cai dat he dieu hanh va bootloader hoan tat!${TX_NC}"
+echo "Nhan nut bat ki de khoi dong lai..."
+read -n 1 -s
+
+exit
